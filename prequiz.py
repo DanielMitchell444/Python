e02f37b259040a32a22c50c15352503ef9791733
Daniel Mitchell <49624246+DanielMitchell444@users.noreply.github.com>
2023-12-11 11:22:09 -0800

    Add files via upload

---
+++ prequiz.py
@@ -0,0 +1,7 @@
+##This is a pre assessment for Python, this is not graded ##
+
+##Create 2 variables using Python, the first one should equal 2 while the other one should equal 2
+
+##Create a list using Python
+
+##Add both variables together and print them
